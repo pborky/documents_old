b97d85a1d8f367ed138bc2137acd61e0e6c2b5b5
Peter Boraros <pborky@pborky.sk>
2011-11-19 15:16:24 +0100

    Almost working on nVidia ION.

--- GPU/filter/filter.py
+++ GPU/filter/filter.py
@@ -1,108 +1,255 @@
 #!/usr/bin/python
 
-import pyopencl as cl
-import numpy as np
-import io
+class PrefixSum:
+    import numpy as np
+    HOST_TYPE_DATA = np.float32
+    HOST_TYPE_KEY = np.uint32
+    
+    GPU_TYPE_DATA = 'float'
+    GPU_TYPE_KEYS = 'uint'
+            
+    LOCAL_DIMS = (16, 16, 1)
+    LOCAL_SIZE = np.prod(LOCAL_DIMS)
+    
+    FILE_NAME = 'kernels.cl'
+    
+    def get_grid_dims(self, ndata):
+        return (int(np.ceil(1.0*ndata/PrefixSum.LOCAL_SIZE)), 1, 1)
+    
+    def get_global(self, grid):
+        return (grid[0]*PrefixSum.LOCAL_DIMS[0], grid[1]*PrefixSum.LOCAL_DIMS[1], grid[2]*PrefixSum.LOCAL_DIMS[2])
+    
+    def __init__(self, fileName = FILE_NAME):
+        import pyopencl as cl
+        import numpy as np
+        import io
+        
+        mf = cl.mem_flags
+        
+        self.ctx = cl.create_some_context(interactive=False, answers=0)
+        self.queue = cl.CommandQueue(self.ctx)
+        
+        f = io.open('kernels.cl')
+        try: lines = f.readlines()
+        finally: f.close()
+        
+        src = ''.join(lines) % { 'LOCAL_SIZE': PrefixSum.LOCAL_SIZE, 'TYPE_DATA':PrefixSum.GPU_TYPE_DATA,'TYPE_KEYS':PrefixSum.GPU_TYPE_KEYS }
+        #print src
+        self.prg = cl.Program(self.ctx, src).build()
 
-mf = cl.mem_flags
+    def filterPrepare(self, data, keys):
+        import numpy as np
+        import pyopencl as cl
+        mf = cl.mem_flags
+        
+        ndata = data.size
+        if keys.size != ndata: raise Exception()
+        
+        filtbytes = np.bool8(False).nbytes * ndata
+        
+        data_buf = cl.Buffer(self.ctx, mf.READ_ONLY, data.nbytes)
+        keys_buf = cl.Buffer(self.ctx, mf.READ_ONLY, keys.nbytes)
+        filt_buf = cl.Buffer(self.ctx, mf.READ_WRITE, filtbytes)
+        
+        e  = [  cl.enqueue_copy(self.queue, data_buf, data), 
+                cl.enqueue_copy(self.queue, keys_buf, keys), ]
+        
+        kernel = self.prg.filterPrepare
+        kernel.set_args(data_buf, keys_buf, np.uint64(ndata), np.uint32(33), np.uint32(66), filt_buf)
+        global_dims = self.get_global(self.get_grid_dims(ndata))
+        e  = [ cl.enqueue_nd_range_kernel(self.queue, kernel, global_dims, PrefixSum.LOCAL_DIMS, wait_for=e), ]
+        
+        return (e, data_buf, keys_buf, filt_buf)
 
-ctx = cl.create_some_context(interactive=False, answers=0)
-queue = cl.CommandQueue(ctx)
+    def prefixSumDown(self, e, filt, ndata):
+        import numpy as np
+        import pyopencl as cl
+        mf = cl.mem_flags
+        
+        if not isinstance(filt, cl.Buffer):
+            filt_buf = cl.Buffer(self.ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf= filt)
+        else:
+            filt_buf = filt
+        
+        databytes = ndata * np.uint64(0).nbytes
+        psumbytes = int(np.ceil(1.0*ndata/PrefixSum.LOCAL_SIZE)) * np.uint64(0).nbytes
+        npsumbytes =  np.uint64(0).nbytes
+        
+        data_buf = cl.Buffer(self.ctx, mf.READ_WRITE, databytes)
+        psum_buf = cl.Buffer(self.ctx, mf.READ_WRITE, psumbytes)
+        npsum_buf = cl.Buffer(self.ctx, mf.READ_WRITE, npsumbytes)
+        
+        kernel = self.prg.prefixSumDown
+        kernel.set_args(filt_buf, data_buf, np.uint64(ndata), psum_buf, npsum_buf)
+        
+        global_dims = self.get_global(self.get_grid_dims(ndata))
+        
+        if e is None:
+            e  = ( cl.enqueue_nd_range_kernel(self.queue, kernel, global_dims, PrefixSum.LOCAL_DIMS, wait_for=e), )
+        else:
+            e  = ( cl.enqueue_nd_range_kernel(self.queue, kernel, global_dims, PrefixSum.LOCAL_DIMS), )
+        
+        return (e, data_buf, psum_buf, npsum_buf)
+    
+    def prefixSumDownInplace(self, e, data, ndata):
+        import numpy as np
+        import pyopencl as cl
+        mf = cl.mem_flags
+        
+        if not isinstance(data, cl.Buffer):
+            data_buf = cl.Buffer(self.ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=data)
+        else:
+            data_buf = data
+        
+        psumbytes = int(np.ceil(1.0*ndata/PrefixSum.LOCAL_SIZE)) * np.uint64(0).nbytes
+        npsumbytes =  np.uint64(0).nbytes
+        
+        psum_buf = cl.Buffer(self.ctx, mf.READ_WRITE, psumbytes)
+        npsum_buf = cl.Buffer(self.ctx, mf.READ_WRITE, npsumbytes)
+        
+        kernel = self.prg.prefixSumDownInplace
+        kernel.set_args(data_buf, np.uint64(ndata), psum_buf, npsum_buf)
+        
+        global_dims = self.get_global(self.get_grid_dims(ndata))
+        
+        if e is None:
+            e  = ( cl.enqueue_nd_range_kernel(self.queue, kernel, global_dims, PrefixSum.LOCAL_DIMS, wait_for=e), )
+        else:
+            e  = ( cl.enqueue_nd_range_kernel(self.queue, kernel, global_dims, PrefixSum.LOCAL_DIMS), )
+        
+        return (e, data_buf, psum_buf, npsum_buf)
+    
+    def prefixSumUp(self, e, data, ndata, data2, ndata2):
+        import numpy as np
+        import pyopencl as cl
+        mf = cl.mem_flags
+        
+        if not isinstance(data, cl.Buffer):
+            data_buf = cl.Buffer(self.ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=data)
+        else:
+            data_buf = data
+        
+        if not isinstance(data2, cl.Buffer):
+            data2_buf = cl.Buffer(self.ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=data2)
+        else:
+            data2_buf = data2
+                
+        kernel = self.prg.prefixSumUp
+        kernel.set_args(data_buf, np.uint64(ndata), data2_buf, np.uint64(ndata2))
+        
+        global_dims = self.get_global(self.get_grid_dims(ndata))
+        
+        if e is None:
+            e  = ( cl.enqueue_nd_range_kernel(self.queue, kernel, global_dims, PrefixSum.LOCAL_DIMS, wait_for=e), )
+        else:
+            e  = ( cl.enqueue_nd_range_kernel(self.queue, kernel, global_dims, PrefixSum.LOCAL_DIMS), )
+        
+        return (e, data_buf, data2_buf)
+    
+    def filter(self, e, data, keys, filt, indices, ndata, ndata2):
+        import numpy as np
+        import pyopencl as cl
+        mf = cl.mem_flags
+        
+        if not isinstance(data, cl.Buffer):
+            data_buf = cl.Buffer(self.ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=data)
+        else:
+            data_buf = data
+        
+        if not isinstance(keys, cl.Buffer):
+            keys_buf = cl.Buffer(self.ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=keys)
+        else:
+            keys_buf = keys
+        
+        if not isinstance(filt, cl.Buffer):
+            filt_buf = cl.Buffer(self.ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=filt)
+        else:
+            filt_buf = filt
+        
+        if not isinstance(indices, cl.Buffer):
+            indices_buf = cl.Buffer(self.ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=indices)
+        else:
+            indices_buf = indices
+        
+        data2bytes = ndata2 * np.uint64(0).nbytes
+        keys2bytes = ndata2 * np.uint64(0).nbytes
+        ndata2bytes = np.uint64(0).nbytes
+        
+        data2_buf = cl.Buffer(self.ctx, mf.READ_WRITE, data2bytes)
+        keys2_buf = cl.Buffer(self.ctx, mf.READ_WRITE, keys2bytes)
+        ndata2_buf = cl.Buffer(self.ctx, mf.READ_WRITE, ndata2bytes)
+        
+        kernel = self.prg.filter
+        kernel.set_args(data_buf, keys_buf, filt_buf, indices_buf, np.uint64(ndata), data2_buf, keys2_buf, ndata2_buf)
+        
+        global_dims = self.get_global(self.get_grid_dims(ndata))
+        
+        if e is None:
+            e  = ( cl.enqueue_nd_range_kernel(self.queue, kernel, global_dims, PrefixSum.LOCAL_DIMS, wait_for=e), )
+        else:
+            e  = ( cl.enqueue_nd_range_kernel(self.queue, kernel, global_dims, PrefixSum.LOCAL_DIMS), )
+        
+        return (e, data2_buf, keys2_buf, ndata2_buf)
 
-HOST_TYPE_DATA = np.float32
-HOST_TYPE_KEY = np.uint32
-
-GPU_TYPE_DATA = 'float'
-GPU_TYPE_KEYS = 'uint'
-
-NDATA =  16777216 #2048
-
-LOCAL_DIMS = (16, 16, 1)
-LOCAL_SIZE = np.prod(LOCAL_DIMS)
-
-SERIAL_COUNT = 1
-GRID_DIMS = (int(np.ceil(np.ceil(1.0*NDATA/LOCAL_SIZE)/SERIAL_COUNT)), 1, 1)
-CACHE_SIZE = np.prod(GRID_DIMS)
-
-subs = {
-    'LOCAL_SIZE':   LOCAL_SIZE,
-    'TYPE_DATA':    GPU_TYPE_DATA,
-    'TYPE_KEYS':    GPU_TYPE_KEYS,
-}
-
-get_global = lambda grid, local: (grid[0]*local[0], grid[1]*local[1], grid[2]*local[2])
-
-f = io.open('kernels.cl')
-try: lines = f.readlines()
-finally: f.close()
-
-src = ''.join(lines) % subs
-print src
-prg = cl.Program(ctx, src).build()
-
-filterPrepare = prg.filterPrepare
-filter = prg.filter
-prefixSumUp = prg.prefixSumUp
-prefixSumDown = prg.prefixSumDown
-
-data = np.random.rand(NDATA).astype(HOST_TYPE_DATA)
-keys = (np.random.rand(NDATA)*100).astype(HOST_TYPE_KEY)
-filtbool = np.zeros(NDATA).astype(np.bool8)
-filtdata = np.zeros(NDATA).astype(np.uint64)
-
-data_buf = cl.Buffer(ctx, mf.READ_ONLY, data.nbytes)
-keys_buf = cl.Buffer(ctx, mf.READ_ONLY, keys.nbytes)
-filtbool_buf = cl.Buffer(ctx, mf.READ_WRITE, filtbool.nbytes)
-filtdata_buf = cl.Buffer(ctx, mf.READ_WRITE, filtdata.nbytes)
-
-e  = [ cl.enqueue_copy(queue, data_buf, data), cl.enqueue_copy(queue, keys_buf, keys), ]
-
-filterPrepare.set_args(data_buf, keys_buf, np.uint64(NDATA), np.uint32(33), np.uint32(66), filtbool_buf, filtdata_buf)
-
-e  = [ cl.enqueue_nd_range_kernel(queue, filterPrepare, get_global(GRID_DIMS, LOCAL_DIMS), LOCAL_DIMS, wait_for=e), ]
-
-e  = [ cl.enqueue_copy(queue, filtbool, filtbool_buf, wait_for=e), ]
-
-print "%d wrong items" % ((keys>=33) & (keys < 66) != filtbool).sum()
-
-psumdata = np.zeros(np.prod(GRID_DIMS)).astype(np.uint64)
-psumndata = np.array([np.prod(GRID_DIMS),], dtype=np.uint64)
-
-psumdata_buf = cl.Buffer(ctx, mf.READ_WRITE, psumdata.nbytes)
-psumndata_buf = cl.Buffer(ctx, mf.READ_WRITE, psumndata.nbytes)
-
-prefixSumDown.set_args(filtdata_buf, np.uint64(NDATA), psumdata_buf, psumndata_buf)
-
-e  = [ cl.enqueue_nd_range_kernel(queue, prefixSumDown, get_global(GRID_DIMS, LOCAL_DIMS), LOCAL_DIMS, wait_for=e), ]
-
-e  = [ cl.enqueue_copy(queue, psumndata, psumndata_buf, wait_for=e), ]
-cl.enqueue_copy(queue, filtdata, filtdata_buf, wait_for=e)
-filtdata[:256]
-
-GRID_DIMS = (int(np.ceil(np.ceil(1.0*psumndata/LOCAL_SIZE)/SERIAL_COUNT)), 1, 1)
-psumdata2 = np.zeros(np.prod(GRID_DIMS)).astype(np.uint64)
-psumndata2 = np.array([0,], dtype=np.uint64)
-
-psumdata2_buf = cl.Buffer(ctx, mf.READ_WRITE, psumdata2.nbytes)
-psumndata2_buf = cl.Buffer(ctx, mf.READ_WRITE, psumndata2.nbytes)
-
-prefixSumDown.set_args(psumdata_buf, np.uint64(psumndata), psumdata2_buf, psumndata2_buf)
-
-e  = [ cl.enqueue_nd_range_kernel(queue, prefixSumDown, get_global(GRID_DIMS, LOCAL_DIMS), LOCAL_DIMS), ]
-
-e  = [ cl.enqueue_copy(queue, psumndata2, psumndata2_buf, wait_for=e), ]
-
-prefixSumUp.set_args(psumdata_buf, np.uint64(psumndata), psumdata2_buf, np.uint64(psumndata2))
-
-GRID_DIMS = (int(np.ceil(np.ceil(1.0*psumndata/LOCAL_SIZE)/SERIAL_COUNT)), 1, 1)
-
-e  = [ cl.enqueue_nd_range_kernel(queue, prefixSumUp, get_global(GRID_DIMS, LOCAL_DIMS), LOCAL_DIMS, wait_for=e), ]
-
-prefixSumUp.set_args(filtdata_buf, np.uint64(NDATA), psumdata_buf, np.uint64(psumndata))
-
-GRID_DIMS = (int(np.ceil(np.ceil(1.0*NDATA/LOCAL_SIZE)/SERIAL_COUNT)), 1, 1)
-
-e  = [ cl.enqueue_nd_range_kernel(queue, prefixSumUp, get_global(GRID_DIMS, LOCAL_DIMS), LOCAL_DIMS, wait_for=e), ]
-
-
-
+if __name__ == '__main__':
+    import numpy as np
+    import pyopencl as cl
+    
+    ps = PrefixSum()
+    
+    NDATA = int(np.exp2(20))
+    
+    data = np.random.rand(NDATA).astype(PrefixSum.HOST_TYPE_DATA)
+    keys = (np.random.rand(NDATA)*100).astype(PrefixSum.HOST_TYPE_KEY)
+    
+    (e, data_buf, keys_buf, filt_buf) = ps.filterPrepare(data, keys)
+    
+    filt = np.zeros(NDATA, dtype = np.bool8)
+    cl.enqueue_copy(ps.queue, filt, filt_buf, wait_for=e)
+    
+    # test #
+    print "Filt:\t%d wrong" % ((keys>=33) & (keys < 66) != filt).sum()
+    ########
+    
+    (e, psum1_buf, psum2_buf, npsum2_buf) = ps.prefixSumDown(e, filt_buf, NDATA)
+    npsum2 = np.zeros(1, dtype = np.uint64)
+    cl.enqueue_copy(ps.queue, npsum2, npsum2_buf, wait_for=e)
+    if npsum2 > 1:
+        (e, psum2_buf, psum3_buf, npsum3_buf) = ps.prefixSumDownInplace(e, psum2_buf, npsum2.item())
+        npsum3 = np.zeros(1, dtype = np.uint64)
+        cl.enqueue_copy(ps.queue, npsum3, npsum3_buf, wait_for=e)
+        if npsum3 > 1:
+            (e, psum3_buf, psum4_buf, npsum4_buf) = ps.prefixSumDownInplace(e, psum3_buf, npsum3.item())
+            npsum4 = np.zeros(1, dtype = np.uint64)
+            psum4 = np.zeros(1, dtype = np.uint64)
+            cl.enqueue_copy(ps.queue, npsum4, npsum4_buf, wait_for=e)
+            cl.enqueue_copy(ps.queue, psum4, psum4_buf, wait_for=e)
+            (e, psum3_buf, psum4_buf) = ps.prefixSumUp(e, psum3_buf, npsum3.item(), psum4_buf, npsum4.item())
+        (e, psum2_buf, psum3_buf) = ps.prefixSumUp(e, psum2_buf, npsum2.item(), psum3_buf, npsum3.item())
+    (e, psum1_buf, psum2_buf) = ps.prefixSumUp(e, psum1_buf, NDATA, psum2_buf, npsum2.item())
+    (e, data2_buf, keys2_buf, ndata2_buf) = ps.filter(e, data_buf, keys_buf, filt_buf, psum1_buf, NDATA, psum4.item())
+    
+    psum1 = np.zeros(NDATA, dtype = np.uint64)
+    ndata2 = np.zeros(1, dtype = np.uint64)
+    data2 = np.zeros(psum4.item(), dtype = PrefixSum.HOST_TYPE_DATA)
+    keys2 = np.zeros(psum4.item(), dtype = PrefixSum.HOST_TYPE_KEY)
+    
+    cl.enqueue_copy(ps.queue, psum1, psum1_buf, wait_for=e)
+    cl.enqueue_copy(ps.queue, ndata2, ndata2_buf, wait_for=e)
+    cl.enqueue_copy(ps.queue, data2, data2_buf, wait_for=e)
+    cl.enqueue_copy(ps.queue, keys2, keys2_buf, wait_for=e)
+    
+    # test #
+    print "CumSum:\t%d wrong" % (filt.cumsum() != psum1).sum()
+    ########
+    
+    keysf = keys[(keys>=33) & (keys < 66)]
+    if keysf.size != keys2.size:
+        print "Result size unmatch"
+    else:
+        wrong = (keys[(keys>=33) & (keys < 66)] != keys2).sum()
+        print "Result:\t%d wrong" % wrong
+        if wrong > 0:
+            i = np.array(xrange(keys.size), dtype=np.int)
+            err = i[keys[(keys>=33) & (keys < 66)] != keys2]
+            print "Total:\t%d\nWrong:\nInd:\t%s\nKeys:\t%s\nKeys2:\t%s" % ( keys2.size, str(err), str(keys[keys[(keys>=33) & (keys < 66)] != keys2]), str(keys2[keys[(keys>=33) & (keys < 66)] != keys2]) )
--- GPU/filter/kernels.cl
+++ GPU/filter/kernels.cl
@@ -1,101 +1,117 @@
-kernel void filterPrepare (
-    global const %(TYPE_DATA)s  *data,
-    global const %(TYPE_KEYS)s  *keys,
-    const ulong                 ndata,
-    const uint                  low,
-    const uint                  high,
-    global bool                 *filter,
-    global ulong                *filtdata
+#define BSIZE (get_local_size(0)*get_local_size(1)*get_local_size(2))
+#define BCOUNT (get_num_groups(0)*get_num_groups(1)*get_num_groups(2))
+#define LID (get_local_id(0) + get_local_size(0)*(get_local_id(1) + get_local_size(1)*get_local_id(2)))
+#define BID (get_group_id(0) + get_num_groups(0)*(get_group_id(1) + get_num_groups(1)*get_group_id(2)))
+#define GID (LID + (BID * BSIZE))
 
-) {    
-    private size_t bsize = get_local_size(0)*get_local_size(1)*get_local_size(2);
-    private size_t bcount = get_num_groups(0)*get_num_groups(1)*get_num_groups(2);
-    
-    private size_t lid = get_local_id(0) + get_local_size(0)*(get_local_id(1) + get_local_size(1)*get_local_id(2));
-    private size_t bid = get_group_id(0) + get_num_groups(0)*(get_group_id(1) + get_num_groups(1)*get_group_id(2));
-    private size_t gid = lid+bid*bsize;
+#define DATA_T %(TYPE_DATA)s
+#define KEYS_T %(TYPE_KEYS)s
+#define LSIZE %(LOCAL_SIZE)d
+
+kernel void filterPrepare (
+    global const DATA_T     *data,
+    global const KEYS_T     *keys,
+    const ulong             ndata,
+    const KEYS_T            low,
+    const KEYS_T            high,
+    global bool             *filter
+) {
+    private size_t gid = GID;
     
     if (gid < ndata) {
-        private %(TYPE_KEYS)s key_p = keys[gid];
-        private bool filt_p = (key_p >= low) && (key_p < high);
+        private KEYS_T key_p = keys[gid];
+        private bool filt_p = ((key_p >= low) && (key_p < high)) ? 1:0;
         filter[gid] = filt_p;
-        filtdata[gid] = filt_p;
     }
 }
+
 kernel void prefixSumDown (
+    global const bool       *dataB,
+    global ulong            *data,
+    const ulong             ndata,
+    global ulong            *psum,
+    global ulong            *npsum
+) {
+    private size_t bsize, lid, gid;
+    bsize = BSIZE;
+    lid = LID;
+    gid = GID;
+
+    local ulong psum_l [LSIZE];
+
+    private ulong psum_p = ((gid < ndata) && dataB[gid]) ? 1 : 0;
+    
+    for ( ulong i = 1; i < bsize ; i <<= 1 ) {
+        psum_l[lid] = psum_p;
+        barrier(CLK_LOCAL_MEM_FENCE);
+        if (lid >= i) {
+            psum_p += psum_l[lid-i];
+        }
+    }
+        
+    if (gid < ndata)    data[gid] = psum_p;
+    if (lid+1 == bsize) psum[BID] = psum_p;
+    if (gid == 0)       *npsum = BCOUNT;
+}
+
+kernel void prefixSumDownInplace (
     global ulong                *data,
     const ulong                 ndata,
     global ulong                *psum,
     global ulong                *npsum
 ) {
-    private size_t bsize = get_local_size(0)*get_local_size(1)*get_local_size(2);
-    private size_t bcount = get_num_groups(0)*get_num_groups(1)*get_num_groups(2);
+    private size_t bsize, lid, gid;
+    bsize = BSIZE;
+    lid = LID;
+    gid = GID;
     
-    private size_t lid = get_local_id(0) + get_local_size(0)*(get_local_id(1) + get_local_size(1)*get_local_id(2));
-    private size_t bid = get_group_id(0) + get_num_groups(0)*(get_group_id(1) + get_num_groups(1)*get_group_id(2));
-    private size_t gid = lid + bid*bsize;
-    
-    local ulong psum_l [%(LOCAL_SIZE)d];
+    local ulong psum_l [LSIZE];
     
     private ulong psum_p = (gid < ndata) ? data[gid] : 0;
-
-    for ( ulong i = 1; i < bsize ; i *= 2 ) {
+    
+    for ( ulong i = 1; i < bsize ; i <<= 1 ) {
         psum_l[lid] = psum_p;
         barrier(CLK_LOCAL_MEM_FENCE);
-        if ((lid >= i) && (gid < ndata)) {
+        if (lid >= i) {
             psum_p += psum_l[lid-i];
         }
     }
-    if (gid < ndata) {
-        data[gid] = psum_p;
-    }
-    if (lid+1 == bsize) {
-        psum[bid] = psum_p;
-    }
-    if (gid == 0) {
-        *npsum = bcount;
-    }
+    
+    if (gid < ndata)    data[gid] = psum_p;
+    if (lid+1 == bsize) psum[BID] = psum_p;
+    if (gid == 0)       *npsum = BCOUNT;
 }
+
 kernel void prefixSumUp (
     global ulong                *data,
     const ulong                 ndata,
     global ulong                *psum,
     const ulong                 npsum
 ) {
-    private size_t bsize = get_local_size(0)*get_local_size(1)*get_local_size(2);
-    private size_t bcount = get_num_groups(0)*get_num_groups(1)*get_num_groups(2);
-    
-    private size_t lid = get_local_id(0) + get_local_size(0)*(get_local_id(1) + get_local_size(1)*get_local_id(2));
-    private size_t bid = get_group_id(0) + get_num_groups(0)*(get_group_id(1) + get_num_groups(1)*get_group_id(2));
-    
-    private size_t gid = lid + (bid+1)*bsize;
+    private size_t bid, gid;
+    bid = BID;
+    gid = GID + BSIZE;
     
     local ulong psum_l;
-    if ((bid < npsum) && (gid < ndata) && (lid == 0)) {
-        psum_l = psum[bid];
+    if (LID == 0) {
+        psum_l = (bid < npsum) ? psum[bid] : 0;
     }
     barrier(CLK_LOCAL_MEM_FENCE);
-    if ((bid < npsum) && (gid < ndata)) {
-        private size_t psum_p = psum_l;
-        data[gid] += psum_p;
+    if (gid < ndata) {
+        data[gid] += psum_l;
     }
 }
 kernel void filter (
-    global const %(TYPE_DATA)s  *data,
-    global const %(TYPE_KEYS)s  *keys,
-    global const bool           *filter,
-    global const ulong          *indices,
-    const ulong                 ndatain,
-    global %(TYPE_DATA)s        *dataout,
-    global %(TYPE_KEYS)s        *keysout,
-    global ulong                *ndataout
+    global const DATA_T     *data,
+    global const KEYS_T     *keys,
+    global const bool       *filter,
+    global const ulong      *indices,
+    const ulong             ndatain,
+    global DATA_T           *dataout,
+    global KEYS_T           *keysout,
+    global ulong            *ndataout
 ) {
-    private size_t bsize = get_local_size(0)*get_local_size(1)*get_local_size(2);
-    private size_t bcount = get_num_groups(0)*get_num_groups(1)*get_num_groups(2);
-    
-    private size_t lid = get_local_id(0) + get_local_size(0)*(get_local_id(1) + get_local_size(1)*get_local_id(2));
-    private size_t bid = get_group_id(0) + get_num_groups(0)*(get_group_id(1) + get_num_groups(1)*get_group_id(2));
-    private size_t gid = lid+bid*bsize;
+    private size_t gid = GID;
     
     if (gid < ndatain) {
         if (filter[gid]) {
@@ -106,5 +122,4 @@ kernel void filter (
         if (gid + 1 == ndatain) *ndataout = indices[gid];
     }
 }
-
 
