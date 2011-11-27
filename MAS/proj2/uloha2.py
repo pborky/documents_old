b7c961983e0fcb1eeb1369d2b01f967ec159ec70
Peter Boraros <pborky@pborky.sk>
2011-11-27 05:31:34 +0100

    add the 2nd homework

---
+++ MAS/proj2/uloha2.py
@@ -0,0 +1,295 @@
+#!/usr/bin/python
+class VertexKind(object):
+    GOLD    = 0x01
+    BARRIER = 0x02
+    EMPTY   = 0x04
+    START   = 0x08
+    FINISH  = 0x10
+    DANGER  = 0x20
+    
+    @classmethod
+    def to_string(cls, val):
+        if not isinstance(val, int):
+            raise TypeError('Expecting integer argument')
+        ret = [ k for (k,v) in cls.__dict__.iteritems() if v == val ]
+        if len(ret) == 0: return None
+        else: return ret[0]
+    
+    def __init__(self, val):
+        if VertexKind.to_string(val) is None:
+            raise TypeError('Unknown token kind')
+        self.val = val
+    def __str__(self):
+        return '%s' % VertexKind.to_string(self.val)
+    def __repr__(self):
+        return '<VertexKind %s>' % str(self)
+    def __eq__(self, other):
+        if isinstance(other, int):
+            return self.val == other
+        elif isinstance(other, VertexKind):
+            return self.val == other.val
+        else:
+            raise TypeError('Wrong comparison')
+
+class Token(object):
+    def __init__(self, row, col, kind):
+        if not isinstance(kind, VertexKind):
+            kind = VertexKind(kind)
+        self.row = row
+        self.col = col
+        self.kind = kind
+    def __str__(self):
+        return '%s at (%d, %d)' % (str(self.kind), self.row, self.col)
+    def __repr__(self):
+        return '<Token %s>' % str(self)
+
+class Vertex(object):
+    def __init__(self, token):
+        if not isinstance(token, Token) and not isinstance(token, Vertex):
+            raise TypeError('Expecting instance of class "Token" or "Vertex"')
+        self.kind = token.kind
+        self.col = token.col
+        self.row = token.row
+        self.edges = []
+        self.dropped = False
+    def add_edge(self, edge):
+        self.edges += edge,
+    def drop(self):
+        for e in list(self.edges):
+            e.drop()
+        self.dropped = True
+    def __str__(self):
+        return '%s at (%d, %d)' % (str(self.kind), self.row, self.col)
+    def __repr__(self):
+        return '<Vertex %s>' % str(self)
+    def __eq__(self, other):
+        if isinstance(other, Vertex):
+            return (self.kind == other.kind) and ((self.row, self.col) == (other.row, other.col))
+        else: return False
+
+
+class Edge(object):
+    def __init__(self, node1, node2):
+        if not isinstance(node1, Vertex) or not isinstance(node2, Vertex):
+            raise TypeError('Expecting instance of class Vertex')
+        self.nodes = (node1, node2)
+        node1.add_edge(self)
+        node2.add_edge(self)
+        self.dropped = False
+    def drop(self):
+        for n in self.nodes:
+            n.edges.remove(self)
+        self.nodes = ()
+        self.dropped = True
+    def other(self, node):
+        return self.nodes[self.nodes.index(node)^1]
+    def __str__(self):
+        return '[%s]_[%s]' % self.nodes
+    def __repr__(self):
+        return '<Edge %s>' % str(self)
+    def __eq__(self, other):
+        if isinstance(other, Edge):
+            if self.nodes == () or other.nodes == ():
+                return self.__class__.__base__.__eq__(other)
+            return (((self.nodes[0] == other.nodes[0]) and (self.nodes[1] == other.nodes[1])) or 
+                   ((self.nodes[0] == other.nodes[1]) and (self.nodes[1] == other.nodes[0])))
+        else: return False
+
+class Graph(object):
+    def __init__(self, edges, verticles, attributes):
+        self.edges = edges
+        self.verticles = verticles
+        self.attributes = attributes
+    def drop(self, item):
+        if isinstance(item, Vertex):
+            item.drop()
+            self.validate()
+        elif isinstance(item, Edge):
+            item.drop()
+            self.validate()
+        else:
+            raise TypeError('Expecting instance of class Vertex or Edge')
+    def filter(self, kind, cls = Vertex, exclude = False):
+        if cls == Vertex:
+            return [ v for v in self.verticles if exclude ^ (v.kind == kind) ]
+        elif cls == Edge:
+            s = set()
+            for vert in ( v for v in self.verticles if exclude ^ (v.kind == kind) ):
+                s.update(vert.edges)
+            return list(s)
+        else:
+            raise TypeError('Expecting class Edge or Vertex')
+    def validate(self):
+        for e in list(self.edges):
+            if e.dropped: self.edges.remove(e)
+        for v in list(self.verticles):
+            if v.dropped: self.verticles.remove(v)
+    def __str__(self):
+        return '%d edges, %d verticles' % (len(self.edges), len(self.verticles))
+    def __repr__(self):
+        return '<Graph %s>' % str(self)
+    def __eq__(self, other):
+        if isinstance(other, Graph):
+            s = list(self.verticles); o = list(other.verticles)
+            for v in self.verticles:
+                if v in other.verticles:
+                    s.remove(v); o.remove(v)
+            ignore = self.attributes['ignore'] if 'ignore' in self.attributes else None
+            if ignore:
+                result = (0 == len( v for v in o+s if not v.kind in ignore ))
+            else:
+                result = 0 == (len( s ) + len( o ))
+            s = list(self.edges); o = list(other.edges)
+            for v in self.edges:
+                if v in other.edges:
+                    s.remove(v); o.remove(v)
+            if ignore:
+                result &= 0 == len(v for v in o+s if not ((v.nodes[0].kind in ignore) or (v.nodes[1].kind in ignore)))
+            else:
+                result &= 0 == (len( s ) + len( o ))
+            return result
+        else: return False
+
+
+class Parser:
+    import sys
+    TOKENS = {
+        '#': VertexKind.BARRIER,
+        'G': VertexKind.GOLD, 
+        '-': VertexKind.EMPTY, 
+        'S': VertexKind.START, 
+        'D': VertexKind.FINISH, 
+        'E': VertexKind.DANGER
+        }
+    def __init__(self, infile = sys.stdin):
+        self.tokens = []
+        self.rowCount = int(infile.readline())
+        self.colCount = int(infile.readline())
+        self.cols = dict((i, []) for i in range(self.colCount))
+        self.rows = dict((i, []) for i in range(self.rowCount))
+        for row in range(self.rowCount):
+            col = 0
+            for token in infile.readline():
+                if col >= self.colCount: break
+                token = Token(row, col, Parser.TOKENS[token])
+                self.cols[col] += token,
+                self.rows[row] += token,
+                self.tokens += token,
+                col += 1
+        self.attributes = { 'thiefs': int(infile.readline()), 'accuracy': float(infile.readline()) }
+    
+    def graph(self):
+        verticles = []
+        edges = []
+        d = {}
+        for t in self.tokens:
+            v = Vertex(t)
+            d[t] = v
+            verticles += v,
+        for (colnum, values) in self.cols.iteritems():
+            tokens = list(values)
+            last = d[tokens.pop(0)]
+            while len(tokens) > 0:
+                v = d[tokens.pop(0)]
+                edges += Edge(last, v),
+                last = v
+        for (rownum, values) in self.rows.iteritems():
+            tokens = list(values)
+            last = d[tokens.pop(0)]
+            while len(tokens) > 0:
+                v = d[tokens.pop(0)]
+                edges += Edge(last, v),
+                last = v
+        return Graph(edges, verticles, self.attributes)
+
+class Main(object):
+    ARGS = [('fileName', str), ('test', )]
+    KW_ARGS = {'--file': 0, '-f': 0, }
+    FLAG_ARGS = { '--test': 1, }
+    MANDATORY_ARGS = [0]
+    MULTISET_ARGS = [1]
+    def __init__(self, argv):
+        self.kwargs, self.args = self.parseArgs(argv)
+    def parseArgs(self, argv):
+        i = 1
+        kwargs = {}
+        args = []
+        mandatory = list(Main.MANDATORY_ARGS)
+        while i < len(argv):
+            if argv[i] in Main.KW_ARGS:
+                arg = Main.KW_ARGS[argv[i]]
+                key = Main.ARGS[arg][0]
+                value = Main.ARGS[arg][1](argv[i+1])
+                if key in kwargs:
+                    if arg not in Main.MULTISET_ARGS:
+                        raise ValueError('Unexpected multiple occurences of argument "%s".' % key)
+                    else:
+                        kwargs[key] += value,
+                else:
+                    kwargs[key] = [ value ]
+                i += 2
+            elif argv[i] in Main.FLAG_ARGS:
+                arg = Main.FLAG_ARGS[argv[i]]
+                key = Main.ARGS[arg][0]
+                if key not in args:
+                    args += key,
+                elif arg in Main.MULTISET_ARGS:
+                    args += key,
+                i += 1
+            else:
+                raise ValueError('Unexpected argument "%s".' % argv[i])
+            if arg in mandatory:
+                mandatory.remove(arg)
+        if len(mandatory) > 0:
+            raise ValueError('Some of the madatory arguments were not found: %s' % 
+                        ', '.join('"%s"'%Main.ARGS[i][0] for i in mandatory))
+        return (kwargs, args)
+            
+    def dfs(self, graph, vertex, visited, solutions):
+        if vertex in visited:
+            return False
+        visited = list(visited)
+        visited += vertex,
+        if vertex.kind == VertexKind.FINISH:
+            solutions += tuple(visited),
+            return True
+        result = False
+        for v in ( e.other(vertex) for e in vertex.edges ):
+            result |= self.dfs(graph, v, visited, solutions)
+        return result
+    def combinations(self, verticles, select):
+        pass
+    def run(self):
+        import io
+        f = io.open(self.kwargs['fileName'][0], 'r')
+        try:
+            p = Parser(f)
+            self.g = p.graph()
+        finally:
+            f.close()
+        for v in self.g.filter(VertexKind.BARRIER):
+            self.g.drop(v)
+        self.g.attributes['ignore'] = VertexKind.EMPTY,
+        start = self.g.filter(VertexKind.START, Vertex)
+        self.solutions = []
+        solutions = []
+        for v in start:
+            self.dfs(self.g, v, [], solutions)
+        for sol in solutions:
+            verticles = [ Vertex(v) for v in sol ]
+            iterate = list(verticles)
+            edges = []
+            last = iterate.pop(0)
+            while len(iterate):
+                v = iterate.pop(0)
+                edges += Edge(last, v),
+                last = v
+            self.solutions += Graph(edges, verticles, self.g.attributes),
+        self.a = [ tuple( (v.row, v.col) for v in s.verticles ) for s in self.solutions ]
+        self.b = self.combinations(self.g.filter(VertexKind.DANGER), self.g.attributes['thiefs'])
+        
+if __name__ == '__main__':
+    import sys
+    main = Main(sys.argv)
+    main.run()
+
