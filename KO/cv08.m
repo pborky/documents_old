6c09b038e8c4e83aca5eac7fa8bc68bc6ce8d2f9
Peter Boraros <pborky@pborky.sk>
2012-04-12 23:01:26 +0200

    .

--- KO/cv08.m
+++ KO/cv08.m
@@ -8,44 +8,42 @@ function [ I ] = cv08 (sumR, sumC, iters)
     l = zeros(nR+nC, nR+nC);
     u = [ zeros(nR), ones(nR,nC);
           zeros(nC,nR), zeros(nC) ];
-    t=' *';
-    img = I;
-    for ic = 1:iters,
+    imx = ceil(sqrt(iters));
+    imy = ceil(iters/imx);
+    for ic = 1:iters
         [c] = fce(cp, I);
         cp = c;
         g = graph;
         F = g.mincostflow(c,l,u,b);
         I = ceil(F(1:nR,nR+1:nR+nC));
-	fprintf('iter=%d\n', ic);
-        fprintf('change=%d\n',sum(sum(abs(I-img))));
-	t(I+1)
-	img = I;
+        subplot(imx, imy, ic);
+        imagesc(logical(I));
+        colormap(gray);
+        axis off;
+        axis square;
+        title(sprintf('%d',ic));
     end;
-    fprintf('diff: R=%f; C=%f;\n', sum(sumR~=sum(I,1)), sum(sumC'~=sum(I,2)));
 
     function [c] = fce (cp, I)
         [nR,nC] = size(I);
         c = zeros(nR+nC, nR+nC);
-        for row = 1:nR,
-            for col = 1:nC,
-                if row == 1 || row == nR || col == 1 || col == nC,
-                    continue;
-                end;
+        for row = 2:nR-1,
+            for col = 2:nC-1,
                 if I(row,col) == 1,
                     s = sum(sum(I(row-1:row+1,col-1:col+1)));
                     if s == 1,
                         c(row,col+nR) = 1;
-                    elseif s == 2.
+                    elseif s == 2,
                         c(row,col+nR) = .2;
-                    elseif s == 3.
+                    elseif s == 3,
                         c(row,col+nR) = .1;
                     end;
                 else
-                    if (I(row-1,col) == 1 && I(row+1,col) == 1) || (I(row,col-1) == 1 && I(row,col+1) == 1),
+                    if (sum(I([row-1,row+1],col)) == 2) || (sum(I(row,[col-1,col+1])) == 2),
                         c(row,col+nR) = -.1;
                     end;
                 end;
             end;
-            c = c + .5*cp;
         end;
+        c = c + .5*cp;
         
